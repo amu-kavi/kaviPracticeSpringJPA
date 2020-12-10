fa35d2d87342291e86929ae0193a5f436160c3c2
Kavitha Murugan <mkmkkavitha@gmail.com>
2020-12-11 00:44:04 +0530

    Query usage from JPA repository

--- libraries/src/main/java/com/ninja/libraries/repositories/UserJpaRepository.java
+++ libraries/src/main/java/com/ninja/libraries/repositories/UserJpaRepository.java
@@ -1,11 +1,13 @@
 package com.ninja.libraries.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ninja.libraries.models.Users;
 
 public interface UserJpaRepository extends JpaRepository<Users,String>{
-
-	//Users findbyUserId(String id);
+   
+	@Query("Select u from Users u where u.userId = ?1")
+	Users findbyUserId(String id);
 
 }
